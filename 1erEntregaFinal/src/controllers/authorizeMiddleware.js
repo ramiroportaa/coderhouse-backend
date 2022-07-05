5fb1c129776d785af152b6f8eb93337141833488
ramiporta <p0rt4csd@gmail.com>
2022-07-05 13:11:33 -0300

    ordenando carpetas

--- 1erEntregaFinal/src/controllers/authorizeMiddleware.js
+++
@@ -1,5 +0,0 @@
-export const authorizeMiddleware = (req, res, next)=>{
-    const rol = req.headers["rol"];
-    if (rol === "admin") return next();
-    return res.status(401).json({ error: -1, descripcion: `ruta ${req.originalUrl} método ${req.method} no autorizada` });
-};
