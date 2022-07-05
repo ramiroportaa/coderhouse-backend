d3377ab40d23613036aca18c0d62498a64dfec5f
ramiporta <p0rt4csd@gmail.com>
2022-07-05 00:15:49 -0300

    configuracion de PORT en .env

--- 1erEntregaFinal/src/server.js
+++ 1erEntregaFinal/src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 import productsRouter from "./routes/productsRouter.js";
 import cartsRouter from "./routes/cartsRouter.js";
 
+if (process.env.NODE_ENV == 'production') {
+    const {config} = await import('dotenv');
+    config();
+}
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -11,6 +16,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/api/productos", productsRouter);
 app.use("/api/carrito", cartsRouter);
+
+app.use((req, res)=>{ res.status(404).json({error: -2, descripcion: `ruta ${req.originalUrl} método ${req.method} no implementada`}) });
 
 const server = app.listen(PORT, ()=> console.log(`Server listening on port: ${PORT}`));
 server.on("error", err => console.log(`Oh no! Something is broken on the server: ${err}`));
